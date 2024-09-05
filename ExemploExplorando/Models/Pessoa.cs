c279cec130f30e613ce31678061acbb931db3990
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-09-05 10:19:33 -0300

    :sparkles: feat: Criando um método.

--- ExemploExplorando/Models/Pessoa.cs
+++ ExemploExplorando/Models/Pessoa.cs
@@ -9,5 +9,10 @@ namespace ExemploExplorando.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
     }
 }
--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Guilherme";
+p1.Idade = 19;
+p1.Apresentar();
