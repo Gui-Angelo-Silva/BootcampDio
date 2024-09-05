dfbcf8980721f9e952472ca0527a9900ca84d169
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-09-05 10:26:42 -0300

    :sparkles: feat: Validações no GET e SET.

--- ExemploExplorando/Models/Pessoa.cs
+++ ExemploExplorando/Models/Pessoa.cs
@@ -7,7 +7,24 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
         public int Idade { get; set; }
 
         public void Apresentar()
