397ab27fbbed90a9e3a316957fb9516ec0b2e5f5
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-09-12 10:50:42 -0300

    :sparkles: feat: Implementando teste unitário.

---
+++ modulotestesdio/CalculadoraTestes/CalculadoraTestes.cs
@@ -0,0 +1,22 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTests
+{
+    private CalculadoraImp _calc;
+
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImp();
+    }
+
+    [Fact]
+    public void DeveSomar5Com10ERetornar15()
+    {
+        // Arrange
+        
+        // Act
+        // Assert
+    }
+}
