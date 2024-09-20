11ed5ba4779db5b0ac3c2d11fbd873965b1816a2
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-09-20 11:06:14 -0300

    :test_tube: test: Criando o primeiro teste de função - somar.

--- NewTalents/TesteNewTalents/UnitTest1.cs
+++ NewTalents/TesteNewTalents/UnitTest1.cs
@@ -1,13 +1,20 @@
 using NewTalents;
+using Xunit;
 
 namespace TesteNewTalents
 {
 	public class UnitTest1
 	{
-		[Fact]
-		public void Test1()
+		[Theory]
+		[InlineData (1, 2, 3)]
+		[InlineData (4, 5, 9)]
+		public void Test1(int val1, int val2, int resultado)
 		{
-			Calculadora cal = new Calculadora();
+			Calculadora calc = new Calculadora();
+
+			int resultadoCalculadora = calc.somar(val1, val2);
+
+			Assert.Equal(resultado, resultadoCalculadora);
 		}
 	}
 }
