d30ec8bb3e292b6b7bb3692fcef06cb2a2000e12
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-09-20 11:01:21 -0300

    :test_tube: test: Parte - 2. Criando projeto de teste e adicionando teste.

--- NewTalents/NewTalents/Calculadora.cs
+++ NewTalents/NewTalents/Calculadora.cs
@@ -8,5 +8,29 @@ namespace NewTalents
 {
 	public class Calculadora
 	{
+		public int somar(int val1, int val2)
+		{
+			return 0;
+		}
+
+		public int subtrair(int val1, int val2)
+		{
+			return 0;
+		}
+
+		public int multiplicar(int val1, int val2)
+		{
+			return 0;
+		}
+
+		public int dividir(int val1, int val2)
+		{
+			return 0;
+		}
+
+		public List<string> historico()
+		{
+			return new List<string>();
+		}
 	}
 }
