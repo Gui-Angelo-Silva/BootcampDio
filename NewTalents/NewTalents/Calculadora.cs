0471f5b2d2c72e764196fef42afff778313a71ca
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-09-20 10:56:47 -0300

    :test_tube: test: Criando projeto de teste e adicionando teste.

---
+++ NewTalents/NewTalents/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTalents
+{
+	public class Calculadora
+	{
+	}
+}
---
+++ NewTalents/TesteNewTalents/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NewTalents;
+
+namespace TesteNewTalents
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			Calculadora cal = new Calculadora();
+		}
+	}
+}
